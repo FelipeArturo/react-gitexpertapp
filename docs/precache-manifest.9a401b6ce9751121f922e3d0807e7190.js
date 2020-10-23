f783838655023eacac61c7c2b6592cedb4fd71ad
felipe <felipecazarez1215@gmail.com>
2020-10-22 19:44:47 -0600

    docs agregados

---
+++ docs/precache-manifest.9a401b6ce9751121f922e3d0807e7190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d0e608ea185bae8861239aded1c97d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0e444a40216cd84b9e5",
+    "url": "/static/css/main.572758bf.chunk.css"
+  },
+  {
+    "revision": "1b9ea6da0ea3cf667ed7",
+    "url": "/static/js/2.a188440b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.a188440b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e444a40216cd84b9e5",
+    "url": "/static/js/main.86bc0828.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
